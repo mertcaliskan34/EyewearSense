6a6848630d86f45f00f742e5a37fae4b46e947fc
Fatih-Atalay <1210606018@nku.edu.tr>
2025-05-02 19:02:56 +0300

    glass type classifier improved model and train code

--- TrainSunglas.py
+++ TrainSunglas.py
@@ -2,11 +2,12 @@ import tensorflow as tf
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
-
+from tensorflow.keras.callbacks import EarlyStopping
 # Paths
 base_dir = 'data'
 img_size = (128, 128)
 batch_size = 32
+early_stop = EarlyStopping(patience=5, restore_best_weights=True)
 
 # Data generators
 train_gen = ImageDataGenerator(rescale=1./255).flow_from_directory(
@@ -27,20 +28,23 @@ val_gen = ImageDataGenerator(rescale=1./255).flow_from_directory(
 model = Sequential([
     Conv2D(32, (3, 3), activation='relu', input_shape=(128, 128, 3)),
     MaxPooling2D(2, 2),
-    
+
     Conv2D(64, (3, 3), activation='relu'),
     MaxPooling2D(2, 2),
-    
+
+    Conv2D(128, (3, 3), activation='relu'),
+    MaxPooling2D(2, 2),
+
     Flatten(),
-    Dense(128, activation='relu'),
+    Dense(256, activation='relu'),
     Dropout(0.5),
-    Dense(1, activation='sigmoid')  # binary: sunglasses vs. normal glasses
+    Dense(1, activation='sigmoid')
 ])
 
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
 # Train
-history = model.fit(train_gen, epochs=10, validation_data=val_gen)
+history = model.fit(train_gen, epochs=30, validation_data=val_gen, callbacks=[early_stop])
 
 # Save
-model.save('glasses_type_classifier.h5')
+model.save('glasses_type_classifierv3.h5')
